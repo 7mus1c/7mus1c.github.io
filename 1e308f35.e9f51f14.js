d3741e2e9c6c572a5b1adbdb10dbd70cb8e1d238
licheng <7mus1c@163.com>
2021-01-12 15:51:32 +0800

    初次提交

--- 1e308f35.e9f51f14.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{75:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"README","permalink":"/blog/README"},{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ sidebars.js
@@ -0,0 +1,8 @@
+// 配置侧边栏文件
+
+module.exports = {
+  someSidebar: {
+    '文档1': ['doc1', 'doc2', 'doc3'],
+    '文档2': ['mdx'],
+  },
+};
